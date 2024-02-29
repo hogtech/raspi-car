c2fbef6a24d60f986f2c68eae2c71c5ecbe121be
Hans Hokka <hans.hokka@protonmail.com>
2024-02-29 15:41:10 +0200

    Simple GET to run servo successful

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+# server.py
+from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message')
 def handle_message(message):
     print('Received message: ' + message)
-    emit('response', 'Message received: ' + message)
+    socketio.emit('response', 'Message received: ' + message)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
--- simpleServerTest.py
+++ simpleServerTest.py
@@ -1,11 +1,13 @@
 # server.py
 from flask import Flask, jsonify
+from servoController import *
 
 app = Flask(__name__)
 
 @app.route('/data', methods=['GET'])
 def get_data():
     data = {'message': 'Hello from Flask server!'}
+    testServo()
     return jsonify(data)
 
 if __name__ == '__main__':
